838fb9f4977316c8af8aa1e13bf7b9dc5b2857f0
Arut Jinadit <60090002@kmitl.ac.th>
2018-10-08 19:21:20 +0700

    add admin sdk

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const express = require('express');
 const app = express();
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+console.log(functions.config().firebase);
 app.get('/', (request, response) => {
-    response.send(`${Date.now()}`);
+    response.send('App created');
 });
 app.get('/hello/:name', (request, response) => {
     let name = request.params.name;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,16 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 const express = require('express')
 
 const app = express()
+admin.initializeApp(functions.config().firebase)
+
+const db = admin.firestore();
+console.log(functions.config().firebase)
+
 
 app.get('/',(request, response) => {
-    response.send(`${Date.now()}`)
+    response.send('App created')
 })
 
 app.get('/hello/:name',(request, response) => {
